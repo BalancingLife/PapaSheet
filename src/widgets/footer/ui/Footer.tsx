278148cbc9af766222a6c8bd490c118d46e3f13b
MJ <alswns8495@naver.com>
2025-08-31 14:14:37 +0900

    ✨ Footer UI 구현 ( 추후 기능 구현 필요 )

---
+++ src/widgets/footer/ui/Footer.tsx
@@ -0,0 +1,28 @@
+export default function Footer() {
+  return (
+    <footer className="fixed bottom-0 left-0 w-full border-t border-[rgb(225,227,225)] bg-[#F8FDFD] px-4 py-2 flex items-center text-gray-600">
+      {/* 좌측 버튼 영역 */}
+      <div className="flex items-center gap-2 ml-5 ">
+        <button className="px-3 py-1 rounded-md bg-white border border-[rgb(225,227,225)] hover:bg-gray-50 transition">
+          +
+        </button>
+        <button className="px-3 py-1 rounded-md bg-white border border-[rgb(225,227,225)] hover:bg-gray-50 transition">
+          -
+        </button>
+      </div>
+
+      {/* 우측 시트 목록 영역 */}
+      <div className="flex items-center gap-2 ml-10 ">
+        <div className="px-3 py-1 rounded-md border border-[rgb(225,227,225)] bg-white cursor-pointer hover:bg-gray-50">
+          시트 1
+        </div>
+        <div className="px-3 py-1 rounded-md border border-[rgb(225,227,225)] bg-white cursor-pointer hover:bg-gray-50">
+          시트 2
+        </div>
+        <div className="px-3 py-1 rounded-md border border-[rgb(225,227,225)] bg-white cursor-pointer hover:bg-gray-50">
+          시트 3
+        </div>
+      </div>
+    </footer>
+  );
+}
